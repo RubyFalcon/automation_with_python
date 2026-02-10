03262784a89c3f0c0be26e406c355ea77b7d84d1
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-02-10 16:08:27 +0000

    bar chart added to transactions 2

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
 wb = xl.load_workbook("transactions.xlsx") #loads xl workbook and loads an workbook object
 sheet = wb["Sheet1"]
 cell  = sheet['a1']
@@ -10,4 +12,14 @@ for row in range(2,sheet.max_row + 1): #start from row 2 to ignore heading and i
     corrected_price_cell = sheet.cell(row, 4)
     corrected_price_cell.value = corrected_price
 
+# Create a sheet in our xl file
+values = Reference(sheet, 
+          min_row=2, 
+          max_row=sheet.max_row,
+          min_col=4,
+          max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, "e2")
 wb.save("transaction2.xlsx")
